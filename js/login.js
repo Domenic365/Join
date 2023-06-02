806101a0759cfd0227806ef4d6ba0ef73d28d6a0
Ahmet Iclier <ahmet.iclier@icloud.com>
2023-06-02 16:51:21 +0200

    added login logo animation

---
+++ js/login.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function initLogin() {
+    setTimeout(() => {
+        let logo = document.querySelector('.logoStart');
+        logo.classList.remove('logoStart');
+        logo.classList.add('logomotion');
+    }, 1000);
+}
