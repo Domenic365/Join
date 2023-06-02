874f3753b24c2b31e7da74a9b634a9ac0b62429a
Domenic365 <domenichergert@icloud.com>
2023-06-02 21:17:37 +0200

    contact

--- js/modules/contact.class.js
+++ js/modules/contact.class.js
@@ -1,5 +1,17 @@
 class Contact {
-    constructor(){
+    firstName;
+    secondName;
+    email;
+    phone;
+
+    /**
+     * 
+     * @param {string} firstName 
+     * @param {string} secondName 
+     * @param {string} email 
+     * @param {number} phone 
+     */
+    constructor(firstName, secondName, email, phone){
         
     }
 }
