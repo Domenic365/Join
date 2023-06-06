b633543c50a62bb811d007b2217e3a50b0602364
Domenic <56897401+Domenic365@users.noreply.github.com>
2023-06-06 19:04:43 +0200

    addtask and contacts links are working

--- js/content_loader.js
+++ js/content_loader.js
@@ -1,13 +1,23 @@
 async function includeHTML() {
-    let includeElements = document.querySelectorAll('[w3-include-html]');
-    for (let i = 0; i < includeElements.length; i++) {
-        const element = includeElements[i];
-        file = element.getAttribute("w3-include-html");
-        let resp = await fetch(file);
-        if (resp.ok) {
-            element.innerHTML = await resp.text();
-        } else {
-            element.innerHTML = 'Page not found';
-        }
+  let includeElements = document.querySelectorAll("[w3-include-html]");
+  for (let i = 0; i < includeElements.length; i++) {
+    const element = includeElements[i];
+    file = element.getAttribute("w3-include-html");
+    let resp = await fetch(file);
+    if (resp.ok) {
+      element.innerHTML = await resp.text();
+    } else {
+      element.innerHTML = "Page not found";
     }
+  }
+}
+
+/**
+ *
+ * @param {string} link relative path to the page to load
+ */
+
+function changeContentHTML(link) {
+  document.getElementById("content").setAttribute("w3-include-html", link);
+  init();
 }
