b7ab9580b59e723eefac4fdafbb8e301f53200b5
Domenic <56897401+Domenic365@users.noreply.github.com>
2023-06-08 23:22:59 +0200

    working on changing contact

--- js/contacts.js
+++ js/contacts.js
@@ -1,18 +1,18 @@
 let contacts = new ContactList();
 
 function contactDivider(letter) {
-  return /*html*/`
+    return /*html*/ `
     <div class="contactDivider">
         <p>${letter}</p>
       <div class="divisonLine"><div></div></div>
     </div>
-  `
+  `;
 }
 
 function loadContactHTML(contact, currentContact) {
-  return /*html*/ `
+    return /*html*/ `
   <div onclick="loadSingleContact(${currentContact})" class="contactCard">
-    <div class="frame79"> ${contact.firstLetters}</div>
+    <div class="contactFirstLetters"> ${contact.firstLetters}</div>
     <div class="contactData">
       <div>${contact.firstName} ${contact.secondName}</div>
       <div>${contact.email}</div>
@@ -25,20 +25,44 @@ async function loadContacts() {
     await changeContentHTML("../../assets/templates/contacts.html");
     let contactlist = document.getElementById("contacts");
     contactlist.innerHTML = "";
-    for (let currentContact = 0; currentContact < contacts.contactList.length; currentContact++) {
-      const lastContactData = contacts.contactList[currentContact - 1]
-      const currentContactData = contacts.contactList[currentContact];
-      if (lastContactData) {
-        if (lastContactData.sortingLetter != currentContactData.sortingLetter) {
-          contactlist.innerHTML += contactDivider(currentContactData.sortingLetter);
+    for (
+        let currentContact = 0;
+        currentContact < contacts.contactList.length;
+        currentContact++
+    ) {
+        const lastContactData = contacts.contactList[currentContact - 1];
+        const currentContactData = contacts.contactList[currentContact];
+        if (lastContactData) {
+            if (
+                lastContactData.sortingLetter !=
+                currentContactData.sortingLetter
+            ) {
+                contactlist.innerHTML += contactDivider(
+                    currentContactData.sortingLetter
+                );
+            }
+        } else {
+            contactlist.innerHTML += contactDivider("A");
         }
-      } else {
-        contactlist.innerHTML += contactDivider("A");
-      }
-      contactlist.innerHTML += loadContactHTML(currentContactData, currentContact);
+        contactlist.innerHTML += loadContactHTML(
+            currentContactData,
+            currentContact
+        );
     }
 }
 
 function loadSingleContact(contactNumber) {
-  
+    let contact = contacts.contactList[contactNumber];
+    document.getElementById("contactInformation").innerHTML =
+        "Contact Information";
+    document.getElementById("editContact").innerHTML = "Edit Contact";
+    document.getElementById("firstLetters").innerHTML = contact.firstLetters;
+    document.getElementById("contactName").innerHTML =
+        contact.firstName + " " + contact.secondName;
+    document.getElementById("contactEmail").innerHTML = contact.email;
+    document.getElementById("contactPhone").innerHTML = contact.phone;
+}
+
+function showContactModal() {
+  let modal = document.getElementById('addContact');
 }
