a6e0ce30c2f0a63fc82c8f2cd82e5f4adfe1317a
Domenic <56897401+Domenic365@users.noreply.github.com>
2023-06-08 13:58:21 +0200

    added contact sorting

--- js/contacts.js
+++ js/contacts.js
@@ -1,21 +1,39 @@
+let contacts = new ContactList();
+
+function contactDivider(letter) {
+  return contactDivider = /*html*/`
+    <div class="contactDivider">
+      <p>${letter}</p>
+      <div class="divisonLine"></div>
+    </div>
+  `
+}
+
+function loadContactHTML(contact) {
+  return /*html*/ `
+  <div class="contactCard">
+    <div class="ContactImage">${contact.firstLetters}</div>
+    <div class="contactData">
+      <p>${contact.firstName} ${contact.secondName}</p>
+      <p>${contact.email}</p>
+    </div>
+  </div>
+  `;
+}
+
 async function loadContacts() {
-<<<<<<< HEAD
-    await changeContentHTML("../templates/contacts.html");
-=======
     await changeContentHTML("../../assets/templates/contacts.html");
->>>>>>> dab0bb7ba9343b5819df2553c52dbf55b6679602
     let contactlist = document.getElementById("contacts");
     contactlist.innerHTML = "";
-    contacts.forEach((contact) => {
-        contactlist.innerHTML += /*html*/ `
-    <div class="contactCard">
-      <div class="ContactImage">${contact.firstLetters}</div>
-      <div class="contactData">
-        <p>${contact.firstName} ${contact.secondName}</p>
-        <p>${contact.email}</p>
-      </div>
-    </div>
-
-    `;
-    });
+    for (let currentContact = 0; currentContact < contacts.contactList.length; currentContact++) {
+      const lastContactData = contacts.contactList[currentContact - 1]
+      const currentContactData = contacts.contactList[currentContact];
+      if (lastContactData) {
+        if (lastContactData.sortingLetter == currentContactData.sortingLetter) {
+        
+        }
+      } else {
+      }
+      contactlist.innerHTML += loadContactHTML(currentContactData);
+    }
 }
--- js/modules/contacts.class.js
+++ js/modules/contacts.class.js
@@ -1,56 +1,79 @@
-let contacts = [
-    new Contact(
-        "Bastian",
-        "Hochfels",
-        "bastian.hochfels@gmail.com",
-        "017658468531"
-    ),
-    new Contact(
-        "Aaron",
-        "Hochfels",
-        "bastian.hochfels@gmail.com",
-        "017658468531"
-    ),
-    new Contact(
-        "Anton",
-        "Hochfels",
-        "bastian.hochfels@gmail.com",
-        "017658468531"
-    ),
-    new Contact(
-        "Sarah",
-        "Hochfels",
-        "bastian.hochfels@gmail.com",
-        "017658468531"
-    ),
-    new Contact(
-        "Anna",
-        "Hochfels",
-        "bastian.hochfels@gmail.com",
-        "017658468531"
-    ),
-    new Contact(
-        "Antonia",
-        "Hochfels",
-        "bastian.hochfels@gmail.com",
-        "017658468531"
-    ),
-    new Contact(
-        "Peter",
-        "Hochfels",
-        "bastian.hochfels@gmail.com",
-        "017658468531"
-    ),
-    new Contact(
-        "Reiner",
-        "Hochfels",
-        "bastian.hochfels@gmail.com",
-        "017658468531"
-    ),
-    new Contact(
-        "Rainer",
-        "Winkler",
-        "bastian.hochfels@gmail.com",
-        "017658468531"
-    ),
-];
+class ContactList {
+    contactList = [
+        new Contact(
+            "Rainer",
+            "Winkler",
+            "rainerwinkler@gmail.com",
+            "017626548632"
+        ),
+        new Contact(
+            "Sabine",
+            "Müller",
+            "sabinemueller@gmail.com",
+            "017834568973"
+        ),
+        new Contact(
+            "Hans",
+            "Schmidt",
+            "hansschmidt@gmail.com",
+            "017456879845"
+        ),
+        new Contact(
+            "Annette",
+            "Kohl",
+            "annettekohl@gmail.com",
+            "017754362881"
+        ),
+        new Contact(
+            "Lukas",
+            "Braun",
+            "lukasbraun@gmail.com",
+            "017843562893"
+        ),
+        new Contact(
+            "Sandra",
+            "Kaiser",
+            "sandrakaiser@gmail.com",
+            "017943562874"
+        ),
+        new Contact(
+            "Markus",
+            "Weber",
+            "markusweber@gmail.com",
+            "017543672894"
+        ),
+        new Contact(
+            "Isabella",
+            "Fischer",
+            "isabellafischer@gmail.com",
+            "017234567894"
+        ),
+        new Contact(
+            "Jan",
+            "Wolf",
+            "janwolf@gmail.com",
+            "017732465798"
+        ),
+        new Contact(
+            "Julia",
+            "Schneider",
+            "juliaschneider@gmail.com",
+            "017878932546"
+        )
+    ];
+
+    constructor() {
+        this.sortContacts();
+    }
+
+    sortContacts() {
+        this.contactList.sort((a, b) => {
+            let x = a.firstName.toLowerCase();
+            let y = b.firstName.toLowerCase();
+            if (x < y) {return -1;}
+            if (x > y) {return 1;}
+            return 0;
+          })
+    }
+}
+
