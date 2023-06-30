dce4d8569956c7bfc00d7feca16eefc2886afd36
Domenic <56897401+Domenic365@users.noreply.github.com>
2023-06-30 15:37:16 +0200

    working on remote storage

--- js/modules/contactList.class.js
+++ js/modules/contactList.class.js
@@ -1,64 +1,18 @@
 class ContactList extends HTMLElement {
     contactList = [
-        new Contact(
-            "Rainer Winkler",
-            "rainerwinkler@gmail.com",
-            "017626548632"
-        ),
-        new Contact("Anna Müller", "anna.mueller@example.com", "0123456789"),
-        new Contact(
-            "Max Mustermann",
-            "max.mustermann@example.com",
-            "9876543210"
-        ),
-        new Contact("Lena Schmidt", "lena.schmidt@example.com", "4567890123"),
-        new Contact("Hans Meier", "hans.meier@example.com", "01234567895"),
-        new Contact(
-            "Julia Schneider",
-            "julia.schneider@example.com",
-            "01234567896"
-        ),
-        new Contact(
-            "Fritz Fischer",
-            "fritz.fischer@example.com",
-            "01234567897"
-        ),
-        new Contact("Sabine Weber", "sabine.weber@example.com", "01234567898"),
-        new Contact(
-            "Michaela Wagner",
-            "michaela.wagner@example.com",
-            "01234567899"
-        ),
-        new Contact(
-            "Martin Becker",
-            "martin.becker@example.com",
-            "012345678910"
-        ),
-        new Contact("Sarah Schulz", "sarah.schulz@example.com", "012345678911"),
-        new Contact(
-            "Thomas Keller",
-            "thomas.keller@example.com",
-            "012345678912"
-        ),
-        new Contact(
-            "Melanie Schmitt",
-            "melanie.schmitt@example.com",
-            "012345678913"
-        ),
-        new Contact("Jan Petersen", "jan.petersen@example.com", "012345678914"),
-        new Contact(
-            "Carolin Krause",
-            "carolin.krause@example.com",
-            "012345678915"
-        ),
+        { name: "domenic" },
+        { name: "daniel" },
+        { name: "max" },
+        { name: "irina" },
+        { name: "alexander" },
+        { name: "waldemar" },
     ];
 
     lastSortingLetter;
 
     constructor() {
         super();
-        this.sortContacts();
-        this.loadContactsToHTML();
+
     }
 
     sortContacts() {
@@ -77,9 +31,17 @@ class ContactList extends HTMLElement {
 
     loadContactsToHTML() {
         this.contactList.forEach((contact) => {
-
             this.appendChild(contact);
         });
+    }
+
+    async saveToRemoteStorage() {
+        await setItem("contactList", this.contactList);
+    }
+
+    async loadFromRemoteStorage() {
+        let res = await getItem("contactList");
+        this.contactList = JSON.parse(res);
     }
 }
 
