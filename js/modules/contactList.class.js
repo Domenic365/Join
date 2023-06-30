84ca56ae8b4ccc9ce6cda1836f647198ad40de7c
Domenic <56897401+Domenic365@users.noreply.github.com>
2023-06-30 20:20:07 +0200

    remote storage is working

--- js/modules/contactList.class.js
+++ js/modules/contactList.class.js
@@ -1,12 +1,64 @@
 class ContactList extends HTMLElement {
     contactList = [
+        new Contact(
+            "Rainer Wnkler",
+            "rainerwinkler@gmail.com",
+            "017626548632"
+        ),
+        new Contact("Anna Müller", "anna.mueller@example.com", "0123456789"),
+        new Contact(
+            "Max Mustermann",
+            "max.mustermann@example.com",
+            "9876543210"
+        ),
+        new Contact("Lena Schmidt", "lena.schmidt@example.com", "4567890123"),
+        new Contact("Hans Meier", "hans.meier@example.com", "01234567895"),
+        new Contact(
+            "Julia Schneider",
+            "julia.schneider@example.com",
+            "01234567896"
+        ),
+        new Contact(
+            "Fritz Fischer",
+            "fritz.fischer@example.com",
+            "01234567897"
+        ),
+        new Contact("Sabine Weber", "sabine.weber@example.com", "01234567898"),
+        new Contact(
+            "Michaela Wagner",
+            "michaela.wagner@example.com",
+            "01234567899"
+        ),
+        new Contact(
+            "Martin Becker",
+            "martin.becker@example.com",
+            "012345678910"
+        ),
+        new Contact("Sarah Schulz", "sarah.schulz@example.com", "012345678911"),
+        new Contact(
+            "Thomas Keller",
+            "thomas.keller@example.com",
+            "012345678912"
+        ),
+        new Contact(
+            "Melanie Schmitt",
+            "melanie.schmitt@example.com",
+            "012345678913"
+        ),
+        new Contact("Jan Petersen", "jan.petersen@example.com", "012345678914"),
+        new Contact(
+            "Carolin Krause",
+            "carolin.krause@example.com",
+            "012345678915"
+        ),
     ];
 
     lastSortingLetter;
 
     constructor() {
         super();
-
+        this.sortContacts();
+        this.loadContactsToHTML();
     }
 
     sortContacts() {
@@ -30,12 +82,28 @@ class ContactList extends HTMLElement {
     }
 
     async saveToRemoteStorage() {
-        await setItem("contactList", JSON.stringify(this.contactList));
+        let contactListForStorage = [];
+        this.contactList.forEach((contact) => {
+            let contactForStorage = {
+                name: contact.name,
+                email: contact.email,
+                phone: contact.phone,
+            };
+            contactListForStorage.push(contactForStorage);
+        });
+        console.log(contactListForStorage);
+        await setItem("contactList", JSON.stringify(contactListForStorage));
     }
 
     async loadFromRemoteStorage() {
         let res = await getItem("contactList");
-        this.contactList = JSON.parse(res);
+        let contactListFromStorage = JSON.parse(res);
+        this.contactList = [];
+        contactListFromStorage.forEach((contact) => {
+            this.contactList.push(
+                new Contact(contact.name, contact.email, contact.phone)
+            );
+        });
     }
 }
 
