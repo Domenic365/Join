74db81c1080eb14dea56bb2f43ecf01c23c18394
Domenic <56897401+Domenic365@users.noreply.github.com>
2023-06-30 14:05:54 +0200

    added global variables

--- js/contacts.js
+++ js/contacts.js
@@ -1,3 +1,14 @@
 /**global variables */
 
 let selectedContact = new Contact("", "", "");
+let contacts = new ContactList();
+let contactDetails = new ContactDetails();
+
+async function loadContactContainer() {
+    await changeContentHTML("../templates/contacts.html");
+    let contactListContainer = document.querySelector("[contact-list]")
+    let contactDetailsContainer = document.querySelector("[contact-details]")
+    
+    contactListContainer.appendChild(contacts);
+    contactDetailsContainer.appendChild(contactDetails);
+}
