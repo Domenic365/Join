c6c6dca1bcd97dbf70bddb1da2c121d35ad0f8ff
Ahmet <130960696+AhmetIclier@users.noreply.github.com>
2023-07-01 13:15:07 +0200

    summary is working
    maybe you have to clear the cache to see it working.

--- js/summary.js
+++ js/summary.js
@@ -1,11 +1,10 @@
 let loadedTasks = []
-
+let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 
 async function initSummary() {
     await taskLoader();
     renderAllTasks();
-    loadAllTasksFromStg();
 }
 
 async function taskLoader() {
@@ -14,19 +13,46 @@ async function taskLoader() {
 }
 
 function renderAllTasks() {
-    let openTasks = document.getElementById('taskSum');
-    let progressTasks = document.getElementById('progressSum');
-    let feedbackTasks = document.getElementById('feedbackSum');
     console.log(loadedTasks);
-    openTasks.innerHTML = `
-        <p>${loadedTasks.length}</p>
-        <span>Tasks in <br> Board</span>
-    `;
-    renderProgressTasks(progressTasks, loadedTasks);
-    renderFeedbackTasks(feedbackTasks, loadedTasks);
+    renderOpenTasks()
+    renderProgressTasks();
+    renderFeedbackTasks();
+    renderprioTasks();
+    renderTodos();
+    renderDoneTasks();
 }
 
-function renderFeedbackTasks(feedbackTasks, loadedTasks) {
+function renderDoneTasks() {
+    let content = document.querySelector('#done-counter');
+    let counter = 0;
+    for (let i = 0; i<loadedTasks.length; i++) {
+        const element = loadedTasks[i];
+        if (element.status === 'done'){
+            counter++;
+        }
+    }
+    content.innerHTML = counter;
+}
+
+function renderTodos() {
+    let content = document.querySelector('.direction-all-tasks > .box-design > .counter > p');
+    let counter = 0;
+    for (let i = 0; i<loadedTasks.length; i++) {
+        const element = loadedTasks[i];
+        if (element.status === 'todo'){
+            counter++;
+        }
+    }
+    content.innerHTML = counter;
+}
+
+function renderOpenTasks() {
+    const openTasks = document.querySelector('#taskSum > p');
+    openTasks.innerHTML = `${loadedTasks.length}`;
+}
+
+function renderFeedbackTasks() {
+    let feedbackTasks = document.getElementById('feedbackSum');
     let counter = 0;
     for (let i = 0; i<loadedTasks.length; i++) {
         const element = loadedTasks[i]
@@ -40,7 +66,8 @@ function renderFeedbackTasks(feedbackTasks, loadedTasks) {
     `;
 }
 
-function renderProgressTasks(progressTasks, loadedTasks) {
+function renderProgressTasks() {
+    let progressTasks = document.getElementById('progressSum');
     let counter = 0;
     for (let i = 0; i<loadedTasks.length; i++) {
         const element = loadedTasks[i]
@@ -52,4 +79,42 @@ function renderProgressTasks(progressTasks, loadedTasks) {
         <p>${counter}</p>
         <span>Tasks in <br> Progress</span>
     `;
+}
+
+function renderprioTasks() {
+    let counter = 0;
+    let dueDate;
+    let content = document.querySelector('.border-urgend-task > p');
+    for (let i = 0; i<loadedTasks.length; i++) {
+        const element = loadedTasks[i]
+        if (element.prio === 'urgent') {
+            counter++;
+            dueDate = element.dueDate;
+        }
+    };
+    content.innerText = `${counter}`;
+    convertToDateObject(dueDate);
+    
+}
+
+function renderDate(day, month, year) {
+    let dueDateBox = document.querySelector('.deadline-task-position > span > b');
+    dueDateBox.innerText = `${month} ${day}, ${year}`;
+}
+
+function formatDate(dateObject) {
+    let day = dateObject.getDate();
+    let month = months[dateObject.getMonth()];
+    let year = dateObject.getFullYear();
+    renderDate(day, month, year);
+}
+
+function convertToDateObject(dueDate) {
+    let date = new Date(dueDate);
+    formatDate(date);
+}
+
+function getDateFromTask(element) {
+    console.log(element.dueDate)
+    return element.dueDate;
 }
