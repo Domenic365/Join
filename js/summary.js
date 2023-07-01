877e51ddad541b3750c38c3d4aa241dd5c0a9f44
Ahmet <130960696+AhmetIclier@users.noreply.github.com>
2023-07-01 11:35:30 +0200

    summary task-counter first row done

--- js/summary.js
+++ js/summary.js
@@ -1,5 +1,5 @@
-let loadedTasks = [];
-console.log(loadedTasks);
+let loadedTasks = []
+
 
 
 async function initSummary() {
@@ -12,18 +12,44 @@ async function taskLoader() {
     let res = await getItem('allTasks');
     loadedTasks = JSON.parse(res);
 }
-console.log(loadedTasks);
 
 function renderAllTasks() {
     let openTasks = document.getElementById('taskSum');
     let progressTasks = document.getElementById('progressSum');
     let feedbackTasks = document.getElementById('feedbackSum');
-
     console.log(loadedTasks);
     openTasks.innerHTML = `
         <p>${loadedTasks.length}</p>
         <span>Tasks in <br> Board</span>
-    `
+    `;
+    renderProgressTasks(progressTasks, loadedTasks);
+    renderFeedbackTasks(feedbackTasks, loadedTasks);
 }
 
-console.log(loadedTasks);
+function renderFeedbackTasks(feedbackTasks, loadedTasks) {
+    let counter = 0;
+    for (let i = 0; i<loadedTasks.length; i++) {
+        const element = loadedTasks[i]
+        if (element.status === 'feedback') {
+            counter++;
+        }
+    };
+    feedbackTasks.innerHTML = `
+        <p>${counter}</p>
+        <span>Awaiting<br>Feedback</span>
+    `;
+}
+
+function renderProgressTasks(progressTasks, loadedTasks) {
+    let counter = 0;
+    for (let i = 0; i<loadedTasks.length; i++) {
+        const element = loadedTasks[i]
+        if (element.status === 'inProgress') {
+            counter++;
+        }
+    };
+    progressTasks.innerHTML = `
+        <p>${counter}</p>
+        <span>Tasks in <br> Progress</span>
+    `;
+}
