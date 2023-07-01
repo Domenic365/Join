c536d18f550fab6f1c2c94bbfe27ecacb2d34809
Domenic <56897401+Domenic365@users.noreply.github.com>
2023-07-01 14:29:34 +0200

    dialog is working

--- main.js
+++ main.js
@@ -3,4 +3,30 @@
 async function init() {
     await includeHTML();
     await updateContacts();
+    loadOutsideClickForModal();
+}
+
+function showModal() {
+    const modal = document.querySelector("dialog");
+    modal.showModal();
+}
+
+function closeModal() {
+    const modal = document.querySelector("dialog");
+    modal.close();
+}
+
+function loadOutsideClickForModal() {
+    let dialog = document.querySelector("dialog");
+    dialog.addEventListener("click", (e) => {
+        const dialogDimensions = dialog.getBoundingClientRect();
+        if (
+            e.clientX < dialogDimensions.left ||
+            e.clientX > dialogDimensions.right ||
+            e.clientY < dialogDimensions.top ||
+            e.clientY > dialogDimensions.bottom
+        ) {
+            dialog.close();
+        }
+    });
 }
