707ae15e9085f47b57e71ce70ebd6ac72005c3df
Ahmet <130960696+AhmetIclier@users.noreply.github.com>
2023-07-02 09:09:39 +0200

    added jsdoc to summary.js

--- js/summary.js
+++ js/summary.js
@@ -1,7 +1,9 @@
 let loadedTasks = []
 let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-
+/**
+ * This function calls renderfunctions to show current status of board
+ */
 async function initSummary() {
     await taskLoader();
     console.log('hallo');
@@ -13,20 +15,17 @@ async function initSummary() {
     renderDoneTasks();
 }
 
+/**
+ * function to load all tasks in loadedTasks variable
+ */
 async function taskLoader() {
     let res = await getItem('allTasks');
     loadedTasks = JSON.parse(res);
 }
 
-// function renderAllTasks() {
-//     renderOpenTasks()
-//     renderProgressTasks();
-//     renderFeedbackTasks();
-//     renderprioTasks();
-//     renderTodos();
-//     renderDoneTasks();
-// }
-
+/**
+ * this functions renders the finished Tasks which are marked as "done"
+ */
 function renderDoneTasks() {
     let content = document.querySelector('#done-counter');
     let counter = 0;
@@ -39,6 +38,9 @@ function renderDoneTasks() {
     content.innerHTML = counter;
 }
 
+/**
+ * function to render all tasks which are marked as "todo"
+ */
 function renderTodos() {
     let content = document.querySelector('.direction-all-tasks > .box-design > .counter > p');
     let counter = 0;
@@ -51,11 +53,17 @@ function renderTodos() {
     content.innerHTML = counter;
 }
 
+/**
+ * function to render all tasks (amount)
+ */
 function renderOpenTasks() {
     let openTasks = document.querySelector('#taskSum > p');
     openTasks.innerHTML = `${loadedTasks.length}`;
 }
 
+/**
+ * function to render all tasks which are waiting for feedback
+ */
 function renderFeedbackTasks() {
     let feedbackTasks = document.getElementById('feedbackSum');
     let counter = 0;
@@ -71,6 +79,9 @@ function renderFeedbackTasks() {
     `;
 }
 
+/**
+ * function to render all tasks which are marked as "inProgress"
+ */
 function renderProgressTasks() {
     let progressTasks = document.getElementById('progressSum');
     let counter = 0;
@@ -86,6 +97,9 @@ function renderProgressTasks() {
     `;
 }
 
+/**
+ * function to render all tasks which are prio "urgent"
+ */
 function renderprioTasks() {
     let counter = 0;
     let dueDate;
@@ -102,11 +116,23 @@ function renderprioTasks() {
     
 }
 
+/**
+ * function to render due date of urgent task
+ * 
+ * @param {number} day - actual day of duedate
+ * @param {string} month - actual month of duedate
+ * @param {number} year - actual year of duedate
+ */
 function renderDate(day, month, year) {
     let dueDateBox = document.querySelector('.deadline-task-position > span > b');
     dueDateBox.innerText = `${month} ${day}, ${year}`;
 }
 
+/**
+ * This functions gets a date object as parameter to read day, month and year to pass (call) it to the rendering function
+ * 
+ * @param {Object} dateObject - to get the needed format for rendering
+ */
 function formatDate(dateObject) {
     let day = dateObject.getDate();
     let month = months[dateObject.getMonth()];
@@ -114,13 +140,13 @@ function formatDate(dateObject) {
     renderDate(day, month, year);
 }
 
+/**
+ * This function gets a date string from html date input and converts it to a date-object for formatting
+ * 
+ * @param {string} dueDate - date string from date input
+ */
 function convertToDateObject(dueDate) {
     let date = new Date(dueDate);
     formatDate(date);
-}
-
-function getDateFromTask(element) {
-    console.log(element.dueDate)
-    return element.dueDate;
 }
 
